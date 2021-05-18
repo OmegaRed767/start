96281d7a7a15fcfccbf6d576255ec39956e6dead
OmegaRed767 <omegared767@gmail.com>
2021-05-18 18:00:50 +0530

    Login details

--- Login.js
+++ Login.js
@@ -1,1 +1,10 @@
 import React from "react";
+import { Text, View, Button } from "react-native";
+const Login = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+export default Login;
